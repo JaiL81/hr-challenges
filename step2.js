8b6a310fdf2d480904ec881720d306ffabd04046
jmlz <jaime.carratala@gft.com>
2018-03-05 17:25:16 +0100

    Añadido npm, bigInt, y unchash

--- step2.js
+++ step2.js
@@ -7,20 +7,34 @@
     FINPARA
     DEVOLVER resultado;
 FINFUNCION 
+hash('javaguay') devuelve 2724207026793
+42201887587301538842321190871795270412537671711761118644170793575
  */
+var bigInt = require("big-integer");
 
  function hash(cadena) {
-    let resultado = 3;
+    let resultado = bigInt(3);
 
     const alfabeto = 'abcdegjkmnoprstuvwyz';
 
     for (let i = 0; i < cadena.length; i++) {
-        resultado = resultado * 31 + alfabeto.indexOf(cadena[i])
+        resultado = resultado.multiply(31).add(alfabeto.indexOf(cadena[i]))
     }
 
     return resultado;
  }
 
- const result = hash('fhilqx');
+ const result = hash('f');
 
- console.log(result)
+ console.log(result.value)
+ console.log(result.toString())
+
+ function unhash(numero) {
+    let resultado; 
+    while (numero > 0) {
+        numero = (numero + 1) / 31
+    }
+
+    return numero;
+ }
+ console.log(unhash(result.value));
