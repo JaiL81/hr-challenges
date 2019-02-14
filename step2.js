27a0dba0b237e6ecdef6b01437fc8640fe12ab05
Jaime <jail81@gmail.com>
2019-02-14 22:56:04 +0100

    Fixed step2

--- step2.js
+++ step2.js
@@ -9,32 +9,61 @@
 FINFUNCION 
 hash('javaguay') devuelve 2724207026793
 42201887587301538842321190871795270412537671711761118644170793575
+​​​​​45463696790021260035501637457001215449014100014682406506
  */
 var bigInt = require("big-integer");
 
- function hash(cadena) {
+function hash(cadena) {
     let resultado = bigInt(3);
 
     const alfabeto = 'abcdegjkmnoprstuvwyz';
 
     for (let i = 0; i < cadena.length; i++) {
-        resultado = resultado.multiply(31).add(alfabeto.indexOf(cadena[i]))
+        resultado = resultado.multiply(bigInt(31)).add(alfabeto.indexOf(cadena[i]))
     }
 
     return resultado;
  }
 
- const result = hash('f');
+ const result2 = hash('zzzz');
 
- console.log(result.value)
- console.log(result.toString())
+ //console.log(result.value)
+ console.log(result2.toString())
 
  function unhash(numero) {
-    let resultado; 
-    while (numero > 0) {
-        numero = (numero + 1) / 31
-    }
+    var alfabeto = "abcdegjkmnoprstuvwyz"; //fhilqx
+    var treinatUno = bigInt(31)
+    var mod = numero.mod(treinatUno);
+    numero = numero.divide(treinatUno);
 
-    return numero;
+    var res = alfabeto.charAt(mod) + "";
+    while (numero > bigInt(3)) {
+        mod = numero.mod(treinatUno);
+        numero = numero.divide(treinatUno);
+        res += alfabeto.charAt(mod);
+    }
+    var res2 = "";
+    for (var i = res.length - 1; i >=0; i--) {
+      res2 += res.charAt(i);
+    }
+    return res2;
+   // return numero;
  }
- console.log(unhash(result.value));
+
+ const result = hash('javaguay');
+ console.log(result.value)
+ var un2 = unhash(bigInt(result.value));
+ var un = unhash(bigInt('42201887587301538842321190871795270412537671711761118644170793575'));
+ var ha = hash(un)
+ console.log(ha.value)
+ console.log(un2);
+ var javalargo = hash('javaguayjavaguayjavaguayjavaguayjavaguayjavaguayjavaguay')
+ console.log(javalargo.value)
+ console.log(javalargo.value.join(''))
+ console.log(unhash(bigInt(javalargo.value.join(''))))
+ console.log(unhash(javalargo))
+ console.log(unhash(hash('javaguayjavaguayjavaguayjavaguayjavaguayjavaguayjavaguay')))
+ console.log(un)
+ console.log(hash(un).value);
+
+ //paso 4 de 6 121621
